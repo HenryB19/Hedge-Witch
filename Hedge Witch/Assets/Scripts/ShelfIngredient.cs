319f10467035ee38b8e16055ff51c515ae4f7fa7
FinnDavis <s213398@students.aie.edu.au>
2023-06-19 15:28:33 +1000

    Main Commit
    added shelf system to main scene
    added fruit ninja system to main scene
    added cauldron system to main scene
    setup a proper prefab system

--- Hedge Witch/Assets/Scripts/ShelfIngredient.cs
+++ Hedge Witch/Assets/Scripts/ShelfIngredient.cs
@@ -4,18 +4,23 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ShelfIngredient : MonoBehaviour
 {
     public GameObject PrefabToInstantiate;
-    public XRInteractionManager xrim;
-    public Transform Shelf;
-    
+    XRInteractionManager xrim;
+    ShelfData shelf;
+
+    private void Start()
+    {
+        xrim = GameObject.Find("XR Interaction Manager").GetComponent<XRInteractionManager>();
+        shelf = GetComponentInParent<ShelfData>();
+    }
 
     public void OnSelectEntered(SelectEnterEventArgs args)
     {
-        GameObject prefab  = Instantiate(PrefabToInstantiate, transform.position, transform.rotation);
+        GameObject prefab = Instantiate(PrefabToInstantiate, transform.position, transform.rotation);
         XRSimpleInteractable xrsi;
         if (prefab.TryGetComponent(out xrsi))
         {
             xrim.SelectEnter(args.interactorObject, xrsi);
-            Shelf.GetComponent<ShelfData>().ingredientList[(int)prefab.GetComponent<Ingredient>().type]--;
+            shelf.ingredientList[(int)prefab.GetComponent<Ingredient>().type]--;
         }
         else
         {
