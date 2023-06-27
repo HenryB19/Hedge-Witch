eeda7297b6303839c851408b99917760837777a0
HenryB19 <102206788+HenryB19@users.noreply.github.com>
2023-06-27 10:31:28 +1000

    Cauldron particle stuff

--- Hedge Witch/Assets/Henry/ParticleTest.cs
+++ Hedge Witch/Assets/Henry/ParticleTest.cs
@@ -5,24 +5,13 @@ using UnityEngine;
 public class ParticleTest : MonoBehaviour
 {
 
-    public ParticleSystem puffParticle;
-    public AudioSource itemSplash;
+    public ParticleSystem splashParticle;
 
-    public bool partPlayed;
 
     private void OnTriggerEnter(Collider other)
     {
-        itemSplash.Play();
-        puffParticle.Play();
-        partPlayed = true;
+        splashParticle.Play();
 
-        Debug.Log("Splash SFX played");
-
-        if (partPlayed)
-        {
-            //puffParticle.Stop();
-            //Debug.Log("Particle is destroyed after 4 seconds?");
-            partPlayed=false;
-        }
     }
+
 }
