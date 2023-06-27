c5cb81dae67411b98221f3083668660fd4ac4fa6
FinnDavis <s213398@students.aie.edu.au>
2023-06-27 10:12:16 +1000

    show labels on hover

--- Hedge Witch/Assets/Lochlan/Scripts/ShelfData.cs
+++ Hedge Witch/Assets/Lochlan/Scripts/ShelfData.cs
@@ -36,12 +36,57 @@ public class ShelfData : MonoBehaviour
 
     }
 
+    bool beingHovered;
+    int hoveredItem;
+    string hoveredItemLabel;
+
+    public void IsHovering(int type)
+    {
+        beingHovered = true;
+        hoveredItem = type;
+        
+        switch (hoveredItem)
+        {
+            case 1:
+                hoveredItemLabel = "Bat Wing";
+                break;
+            case 2:
+                hoveredItemLabel = "Snake Tongue";
+                break;
+            case 3:
+                hoveredItemLabel = "Dog Heart";
+                break;
+            case 4:
+                hoveredItemLabel = "Rose Thorn";
+                break;
+            case 5:
+                hoveredItemLabel = "Odd Mushroom";
+                break;
+            case 6:
+                hoveredItemLabel = "Unicorn Blood";
+                break;
+            case 7:
+                hoveredItemLabel = "Angel Tears";
+                break;
+            case 8:
+                hoveredItemLabel = "Demon Flesh";
+                break;
+            default:
+                hoveredItemLabel = "Frog Leg";
+                break;
+        }
+    }
+
+    public void NotHovering()
+    {
+        beingHovered = false;
+    }
+
     private void Update()
     {
         // this will cause issues if canvasText count is different from ingredientContainers count.
         for (int i = 0; i < canvasText.Count; i++)
         {
-            
             if (ingredientList[i] <= 0)
             {
                 ingredientContainers[i].gameObject.SetActive(false);
@@ -54,7 +99,9 @@ public class ShelfData : MonoBehaviour
                 canvasText[i].text = ingredientList[i].ToString();
             }
         }
-
-
+        if (beingHovered)
+        {
+            canvasText[hoveredItem].text = hoveredItemLabel;
+        }
     }
 }
